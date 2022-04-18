dcc86ec7ddba5a1652c1542171d8e16db361343b
ACE <elshaer118@gmail.com>
2022-04-18 21:27:03 +0200

    added counter test (the counter assume getting interrupt each 1 second)

--- Stop Watch/Stop Watch/main.c
+++ Stop Watch/Stop Watch/main.c
@@ -26,7 +26,51 @@ void interrupt2_Init();
 void timer1_init();
 
 ISR (TIMER1_COMPA_vect){
-	PORTC ^= 0x07;	
+	static uint16_t s1 = 0, s2 = 0, m1 = 0, m2 = 0;
+	PORTC ^= 0x07;
+	/*
+	if(s1 < 9)
+	{s1++;
+		writeSeg1(s1);
+	}
+	else
+	{
+		s1 = 0;
+		writeSeg1(s1);
+		if(s2 < 6)
+		{
+			s2++;
+			writeSeg2(s2);
+		}
+		else
+		{
+			s2 = 0;
+			writeSeg2(s2);
+			if(m1 < 9)
+			{
+				m1++;
+				writeSeg3(m1);
+			}
+			else
+			{
+				m1 = 0;
+				writeSeg3(m1);
+				if(m2 < 6)
+				{
+					m2++;
+					writeSeg4(m2);
+				}
+				else
+				{
+					m2 = 0;
+					writeSeg4(m2);
+				}
+			}
+			
+		}
+		
+	}
+*/	
 }
 
 int main(void)
@@ -95,3 +139,4 @@ void interrupt2_Init()
 	}
 	
 }
+
