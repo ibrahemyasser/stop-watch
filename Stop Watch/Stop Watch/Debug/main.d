181fbd99c2c02c1ec4504014cbe0fe0dc6c492e3
ACE <elshaer118@gmail.com>
2022-04-24 14:29:59 +0200

    clean code 'using libraries'

--- Stop Watch/Stop Watch/Debug/main.d
+++ Stop Watch/Stop Watch/Debug/main.d
@@ -1,46 +1,51 @@
 main.d main.o: .././main.c \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom32.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ E:\IEEE\Empedded\what\ you\ need\programs\Atmel_Studio_7.0.1645x86-x64\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom32.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ .././header.h .././interrupt.h
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
 
-C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom32.h:
+E:\IEEE\Empedded\what\ you\ need\programs\Atmel_Studio_7.0.1645x86-x64\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom32.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+e:\ieee\empedded\what\ you\ need\programs\atmel_studio_7.0.1645x86-x64\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+.././header.h:
+
+.././interrupt.h:
--- Stop Watch/Stop Watch/main.c
+++ Stop Watch/Stop Watch/main.c
@@ -8,22 +8,9 @@
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "header.h"
+#include "interrupt.h"
 
-#define SET_BIT(reg, bit)	(reg|= 1<<bit)
-#define CLR_BIT(reg, bit)	(reg&= ~(1<<bit))
-
-#define Falling		0
-#define Rising		1
-
-#define I0	Falling
-#define I1	Rising
-#define I2	Falling
-
-void interrupt0_Init();
-void interrupt1_Init();
-void interrupt2_Init();
-
-void timer1_init();
 
 ISR (TIMER1_COMPA_vect){
 	static uint16_t s1 = 0, s2 = 0, m1 = 0, m2 = 0;
@@ -84,59 +71,3 @@ int main(void)
 
     }
 }
-void timer1_init(){
-	// enables OCF1A (output compare match flag)
-	TIFR |= (1<<4);
-	//enables OCIE1A (output compare match interrupt enable)
-	TIMSK |= (1<<4);
-	// enable global interrupt
-	sei();
-	// CTC mode with prescaler 256
-	TCCR1A = 0x00;
-	TCCR1B |=  (1<<2) | (1<<3);
-	// output compare value for a delay of .5 seconds
-	OCR1A = 31249;
-}
-
-void interrupt0_Init()
-{
-	if(I0 == Falling)
-	{
-	SET_BIT(MCUCR , 1); //ISC01
-	CLR_BIT(MCUCR, 0);	//ISC00	
-	}
-	else if (I0 == Rising)	
-	{	
-		SET_BIT(MCUCR, 1);//ISC01
-		SET_BIT(MCUCR, 0);//ISC00
-	}
-
-}
-
-void interrupt1_Init()
-{
-	if(I1 == Falling)
-	{
-		SET_BIT(MCUCR , 3);//ISC11
-		CLR_BIT(MCUCR, 2);//ISC10
-	}
-	else if (I1 == Rising)
-	{
-		SET_BIT(MCUCR, 2);
-		SET_BIT(MCUCR, 3);
-	}
-}
-
-void interrupt2_Init()
-{
-	if (I2 == Falling)
-	{
-	CLR_BIT(MCUCSR, 6);	//ISC2
-	}
-	else if (I2 == Rising)
-	{
-		SET_BIT(MCUCSR, 6)
-	}
-	
-}
-
